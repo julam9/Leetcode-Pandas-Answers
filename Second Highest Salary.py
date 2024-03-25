59b651405b7a0ffb63fa428c4aa68ac8d0f8a9b1
Julio Antonio <julioam766@gmail.com>
2024-03-26 01:18:46 +0700

    first commit

---
+++ Second Highest Salary.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    if employee["salary"].nunique() > 1:
+        return pd.DataFrame(data = {"SecondHighestSalary" : employee[employee["salary"] < employee["salary"].max()].sort_values(by="salary", ascending=False)["salary"].head(1)})
+    else:
+        return pd.DataFrame(data = {"SecondHighestSalary" : None}, index=[" "]) 
