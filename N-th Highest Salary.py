95d3fd2b4a791d28a622d5cb9492cbadaafc2bbc
Julio Antonio <julioam766@gmail.com>
2024-03-29 00:22:47 +0700

    modified:   N-th Highest Salary.py

--- N-th Highest Salary.py
+++ N-th Highest Salary.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
     if employee["salary"].nunique() > N:
-        
+        return pd.DataFrame(data={f"getNthHighestSalary({N})" : employee.sort_values(by="salary", ascending=False)}) 
+    else:
+        return pd.DataFrame(data={f"getNthHighestSalary{N}" : None}, index=" ")
