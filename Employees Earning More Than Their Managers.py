1d92f9653e6bbaee445858dedd82e5f7154b367d
Julio Antonio <julioam766@gmail.com>
2024-04-03 02:32:11 +0700

    modified:   Employees Earning More Than Their Managers.py

--- Employees Earning More Than Their Managers.py
+++ Employees Earning More Than Their Managers.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 def find_employees(employees:pd.DataFrame) -> pd.DataFrame:
-    employees["manager_salary"]
+    e2 = pd.merge(employees, employees, left_on="managerId", right_on="id", how="left", suffixes=["1", "2"])
+    return e2 [e2["salary1"] > e2["salary2"]]["name1"]
+
+aa=pd.DataFrame(data={"id":[1,2,3,4], "name" : ["A", "B", "C", "D"], "salary":[7000,8000,6000,9000], "managerId":[3,4,"Null","Null"]})
+print(find_employees(aa)) 
