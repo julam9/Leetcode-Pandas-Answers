f5b43c208254319f0328daaffd6a68f238021be7
Julio Antonio <julioam766@gmail.com>
2024-04-06 23:57:42 +0700

    modified:   Duplicate Emails.py

--- Duplicate Emails.py
+++ Duplicate Emails.py
@@ -1,4 +1,7 @@
 import pandas as pd 
 
 def duplicate_emails(person:pd.DataFrame) -> pd.DataFrame:
-    return person["email"].value_counts().reset_index()
+    return person["email"].value_counts().reset_index().query("count > 1")
+
+#A = pd.DataFrame(data={"A" : [1,1,1,2,2,3,3,5,0]})
+#print(A.value_counts().reset_index().query("count > 1"))
