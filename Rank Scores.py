c0cab1c60a33aae0a617b19364c722e35e8d1ad0
Julio Antonio <julioam766@gmail.com>
2024-04-11 00:27:25 +0700

    modified:   Rank Scores.py

--- Rank Scores.py
+++ Rank Scores.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 def order_scores(scores: pd.DataFrame) -> pd.DataFrame :
-    return sorted(scores, key="score", reverse=True)
+    d2 = scores.sort_values(by="score", ascending=False)
+    d2["rank"] = d2["score"].rank(method="dense", ascending=False).astype("int")
+    return d2.iloc[:, 1:]
