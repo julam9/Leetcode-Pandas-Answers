a3d5e6cd7fdbdd210975ce0a76d54457bf346f26
Julio Antonio <julioam766@gmail.com>
2024-04-11 22:58:59 +0700

    modified:   Customers Who Never Orders.py

--- Customers Who Never Orders.py
+++ Customers Who Never Orders.py
@@ -2,4 +2,9 @@ import pandas as pd
 
 class Solution():
     def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-        return customers["name"]
+        
+        # join the customers and orders table using left join 
+        d2 = pd.merge(customers, orders, how="left", left_on="id", right_on="customerId")
+        
+        # return the row where the id from order table is null (alternatively, you can use the customerId from order table to checking the null row)
+        return d2[d2["id_y"].isnull()].iloc[:, 1:2].rename(columns = {"name" : "Customers"})
