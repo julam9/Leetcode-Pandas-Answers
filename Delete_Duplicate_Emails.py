535cbdb6862ecf4fb6ae35ff8830bcc6296ec69e
Julio Antonio <julioam766@gmail.com>
2024-04-14 16:15:32 +0700

    renamed:    Consecutive Numbers.py -> Consecutive_Numbers.py 	renamed:    Customers Who Never Orders.py -> Customers_Who_Never_Orders.py 	new file:   Delete_Duplicate_Emails.py 	renamed:    Department Highest Salary.py -> Department_Highest_Salary.py 	new file:   Department_Top_Three_Salaries.py 	renamed:    Duplicate Emails.py -> Duplicate_Emails.py 	renamed:    Employees Earning More Than Their Managers.py -> Employees_Earning_More_Than_Their_Managers.py 	renamed:    N-th Highest Salary.py -> N-th_Highest_Salary.py 	renamed:    Rank Scores.py -> Rank_Scores.py 	renamed:    Second Highest Salary.py -> Second_Highest_Salary.py

---
+++ Delete_Duplicate_Emails.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
---
+++ Department_Top_Three_Salaries.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
