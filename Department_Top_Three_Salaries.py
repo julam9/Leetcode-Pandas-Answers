5983f963f74a2643deca4abff975d674648be5da
Julio Antonio <julioam766@gmail.com>
2024-04-16 01:42:35 +0700

    modified:   Department_Top_Three_Salaries.py

--- Department_Top_Three_Salaries.py
+++ Department_Top_Three_Salaries.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 class Solution():
     def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
-        ranking_salaries = employee.rank()
+        salaries_data = employee.merge(department, how="inner", left_on="departmentId", right_on = "id", suffixes=["", "_y"]).rename(columns={"name" : "Employee", "name_y" : "Department", "salary" : "Salary"}).drop(columns=["id", "id_y", "departmentId"])
+        salaries_data["salary_rank"] = salaries_data.groupby("Department")["Salary"].rank(method="dense", ascending=False)
+        return salaries_data.query("salary_rank < 4").loc[:, ["Department", "Employee", "Salary"]]
