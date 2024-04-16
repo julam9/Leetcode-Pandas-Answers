1b3373a348ddc52c087d8ea945022f12b4677fa4
Julio Antonio <julioam766@gmail.com>
2024-04-16 23:57:44 +0700

    modified:   Delete_Duplicate_Emails.py

--- Delete_Duplicate_Emails.py
+++ Delete_Duplicate_Emails.py
@@ -1,3 +1,8 @@
 import pandas as pd
 
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    '''
+    select only email with concurences equal to 1
+    '''
+    
+    return pd.DataFrame(data={"email" : person["email"].value_counts().reset_index().query("count > 1").iloc[:, 0]})
