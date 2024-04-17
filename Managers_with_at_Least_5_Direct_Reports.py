dfb643a47c24dd58c38484d117b3f5385c01cd95
Julio Antonio <julioam766@gmail.com>
2024-04-18 00:32:17 +0700

    modified:   Managers_with_at_Least_5_Direct_Reports.py

--- Managers_with_at_Least_5_Direct_Reports.py
+++ Managers_with_at_Least_5_Direct_Reports.py
@@ -3,5 +3,4 @@ import pandas as pd
 class Solution():
     def find_managers(employee:pd.DataFrame) -> pd.DataFrame:
         manager_data = pd.merge(employee, employee, left_on = "managerId", right_on="id", right_suffix="_y").rename(columns={"name_y" : "manager_name"})
-        return manager_data["manager_name"].value_counts().reset_index().query(" count > 4")
-        
+        return manager_data["manager_name"].value_counts().reset_index().query(" count > 4").drop()
