90f8c17afa888574a8b4197867056e288073d4c5
Julio Antonio <julioam766@gmail.com>
2024-04-23 00:12:26 +0700

    new file:   Sales_Person.py

---
+++ Sales_Person.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+class Solution():
+    def sales_person(sales_person:pd.Dataframe, company:pd.DataFrame, orders:pd.DataFrame) -> pd.DataFrame:
+        sales_order = pd.merge(sales_person, orders, how='left', on='sales_id') 
+        full_table = pd.merge(sales_order, company, how='inner', on='com_id')
