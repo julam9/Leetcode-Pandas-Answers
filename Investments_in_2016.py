fbf98b6635275e8dec823fb97264d654a9bbacda
Julio Antonio <julioam766@gmail.com>
2024-04-28 23:53:02 +0700

    modified:   Investments_in_2016.py

--- Investments_in_2016.py
+++ Investments_in_2016.py
@@ -2,5 +2,4 @@ import pandas as pd
 
 class Solution():
     def find_investments(insurance:pd.DataFrame) -> pd.DataFrame:
-        rule1_pid = insurance[insurance["tiv_2015"]]
-        rule2_pid = insurance
+        rule1_pid = insurance.groupby("pid")["inv_2015"].count()
