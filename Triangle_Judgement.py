479cf637dd359716d43f30d2290dab7c9304b525
Julio Antonio <julioam766@gmail.com>
2024-05-02 00:01:44 +0700

    modified:   Triangle_Judgement.py

--- Triangle_Judgement.py
+++ Triangle_Judgement.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 class Solution:
     def triangle_judgement(triangle: pd.DataFrame) -> pd.DataFrame:
-        triangle["triange"] = "Yes" if triangle.x < triangle.y < triangle.z else "No"
+        triangle["triangle"] = "Yes" if all([triangle.x + triangle.y > triangle.z, triangle.x + triangle.z > triangle.y, triangle.y + triangle.z > triangle.x]) else "No"
+        return triangle 
