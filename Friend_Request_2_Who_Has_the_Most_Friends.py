f052127ffb5ebad6e36665cd09eaf64827b70e9a
Julio Antonio <julioam766@gmail.com>
2024-05-03 00:30:41 +0700

    modified:   Friend_Request_2_Who_Has_the_Most_Friends.py

--- Friend_Request_2_Who_Has_the_Most_Friends.py
+++ Friend_Request_2_Who_Has_the_Most_Friends.py
@@ -3,3 +3,4 @@ import pandas as pd
 class Solution:
     def most_friends(request_accepted:pd.DataFrame) -> pd.DataFrame:
         request_n = request_accepted["requester_id"].value_counts().reset_index()
+        accept_n = request_accepted["accepter_id"].value_counts().reset_index()
