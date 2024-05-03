7c6cbe12e2b537a42a6a2fe8b7b75b95069cc56a
Julio Antonio <julioam766@gmail.com>
2024-05-03 23:58:09 +0700

    new file:   Exchange_Seats.py

---
+++ Exchange_Seats.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+class Solution:
+    def exchange_seats(seat:pd.DataFrame) -> pd.DataFrame:
+        if len(exchange_seats)%2 == 0:
+            exchange_seats["new_id"] = exchange_seats["id"]-1 if exchange_seats%2==0 else exchange  
