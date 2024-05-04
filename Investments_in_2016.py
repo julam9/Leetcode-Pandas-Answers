236311b8e78d9202ab6df787d9ba81a9e4acd1ca
Julio Antonio <julioam766@gmail.com>
2024-05-04 23:54:37 +0700

    modified:   Investments_in_2016.py 	modified:   Triangle_Judgement.py

--- Investments_in_2016.py
+++ Investments_in_2016.py
@@ -3,3 +3,4 @@ import pandas as pd
 class Solution:
     def find_investments(insurance:pd.DataFrame) -> pd.DataFrame:
         rule1_pid = insurance.groupby("pid")["inv_2015"].count()
+        return rule1_pid
--- Triangle_Judgement.py
+++ Triangle_Judgement.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 class Solution:
     def triangle_judgement(triangle: pd.DataFrame) -> pd.DataFrame:
-        triangle["triangle"] = "Yes" if all([triangle.x + triangle.y > triangle.z, triangle.x + triangle.z > triangle.y, triangle.y + triangle.z > triangle.x]) else "No"
+        triangle["triangle"] = "Yes" if sum([triangle.x + triangle.y > triangle.z, triangle.x + triangle.z > triangle.y, triangle.y + triangle.z > triangle.x]) == 3 else "No"
         return triangle 
