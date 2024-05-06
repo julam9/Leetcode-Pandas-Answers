1a54b58f00ee40ce65b1bdf001add0fcd53ee99c
Julio Antonio <julioam766@gmail.com>
2024-05-07 00:03:44 +0700

    modified:   Movie_Rating.py

--- Movie_Rating.py
+++ Movie_Rating.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 class Solution:
     def movie_rating(movies: pd.DataFrame, users: pd.DataFrame, movie_rating: pd.DataFrame) -> pd.DataFrame:
-        return movie_rating[movie_rating["created_at"] betwe]
+        return movie_rating.query(" '2020-02-01' <=  crated_at <= '2020-02-29' ")
