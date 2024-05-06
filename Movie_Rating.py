8c5b78213689ab97d7cf28fc2401e472d5d80d40
Julio Antonio <julioam766@gmail.com>
2024-05-06 23:57:22 +0700

    new file:   Movie_Rating.py

---
+++ Movie_Rating.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+class Solution:
+    def movie_rating(movies: pd.DataFrame, users: pd.DataFrame, movie_rating: pd.DataFrame) -> pd.DataFrame:
+        return movie_rating[movie_rating["created_at"] betwe]
